4391cd9ba51f49db3a332c63152b7719a3adc605
btimeo <timeo.baffreauleroux@ynov.com>
2025-09-15 10:25:11 +0200

    Mise a jour : Takepot marche

--- src/inventaire.go
+++ src/inventaire.go
@@ -1,19 +1,37 @@
 package red
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Inventory struct {
-	possion int
+	potion int
 }
 
 func initInventory() Inventory {
 	var inventaire_player1 Inventory
-	inventaire_player1.possion = 3
+	inventaire_player1.potion = 3
 	return inventaire_player1
 }
 
 
-func AccessInventory() {
-	inventaire1_player1 := initInventory()
-	fmt.Printf("\n Possion = %v\n", inventaire1_player1.possion)
+func AccessInventory(player1 Character) {
+	fmt.Printf("\n Potion = %v\n", player1.inventaire.potion)
+}
+
+func TakePot(player1 Character) {
+	
+	if player1.inventaire.potion > 0 {
+		player1.pv += 50
+		player1.inventaire.potion -= 1
+		if player1.pv > player1.pv_max {
+			player1.pv = player1.pv_max
+		}
+		DisplayInfo(player1)
+		fmt.Printf("Vous disposez de %v pv sur %v pv\n", player1.pv, player1.pv_max)
+		
+	} else {
+		fmt.Printf("Vous ne disposez pas de potion dans votre inventaire.\n")
+	}
+	
 }
