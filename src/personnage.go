3a9c746c4a78cc67aa2edab93eda5fa4cf2de858
btimeo <timeo.baffreauleroux@ynov.com>
2025-09-15 10:25:42 +0200

    Mise a jour : Takepot marche

--- src/personnage.go
+++ src/personnage.go
@@ -10,21 +10,22 @@ type Character struct {
 	niveau int
 	pv_max int
 	pv     int
+	inventaire Inventory
 }
 
-func initCharacter() Character {
+func InitCharacter() Character {
 	var player1 Character
 	player1.nom = "Rudolf"
 	player1.classe = "Elfe"
 	player1.niveau = 1
 	player1.pv_max = 100
 	player1.pv = 40
+	player1.inventaire = initInventory()
 	return player1
 }
 
-func DisplayInfo() {
-	player1 := initCharacter()
+func DisplayInfo(player1 Character) {
 	fmt.Printf(" Nom : %v\n Classe : %v\n Niveau : %v\n Pv_max : %v\n Pv : %v\n\n", player1.nom, player1.classe, player1.niveau, player1.pv_max, player1.pv)
 	fmt.Printf("Inventaire : ")
-	AccessInventory()
+	AccessInventory(player1)
 }
