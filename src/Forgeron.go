8a55e6b4da2b7868c7738561ccd0debadf1c7786
btimeo <timeo.baffreauleroux@ynov.com>
2025-09-16 12:53:11 +0200

    Mise a jour : Ajout du menu Marchand

--- src/Forgeron.go
+++ src/Forgeron.go
@@ -7,20 +7,20 @@ import (
 
 func Menu_forgeron(player1 Character) Character {
 	var i int
-	Printfct("Bienvenu dans chez le forgeron, que voulez vous fabriquer : \n\n1: Chapeau de l’aventurier (1 Plume de Corbeau et 1 Cuir de Sanglier)\n2: Tunique de l’aventurier (2 Fourrure de loup et 1 Peau de Troll)\n3: Bottes de l’aventurier (1 Fourrure de loup et 1 Cuir de Sanglier)\n\n0: Exit", 30, 5)
+	Printfct("Bienvenu chez le forgeron, que voulez vous fabriquer : \n\n1: Chapeau de l’aventurier (1 Plume de Corbeau et 1 Cuir de Sanglier)\n2: Tunique de l’aventurier (2 Fourrure de loup et 1 Peau de Troll)\n3: Bottes de l’aventurier (1 Fourrure de loup et 1 Cuir de Sanglier)\n\n0: Exit", 30, 5)
 	for {
 		fmt.Scanln(&i)
 		switch i {
 		case 0:
 			return player1
 		case 1:
-			Chapeau_de_l_aventurier(player1)
+			player1 = Chapeau_de_l_aventurier(player1)
 			Printfct("Bienvenu dans chez le forgeron, que voulez vous fabriquer : \n\n1: Chapeau de l’aventurier (1 Plume de Corbeau et 1 Cuir de Sanglier)\n2: Tunique de l’aventurier (2 Fourrure de loup et 1 Peau de Troll)\n3: Bottes de l’aventurier (1 Fourrure de loup et 1 Cuir de Sanglier)\n\n0: Exit", 1, 7)
 		case 2:
-			Tunique_de_l_aventurier(player1)
+			player1 = Tunique_de_l_aventurier(player1)
 			Printfct("Bienvenu dans chez le forgeron, que voulez vous fabriquer : \n\n1: Chapeau de l’aventurier (1 Plume de Corbeau et 1 Cuir de Sanglier)\n2: Tunique de l’aventurier (2 Fourrure de loup et 1 Peau de Troll)\n3: Bottes de l’aventurier (1 Fourrure de loup et 1 Cuir de Sanglier)\n\n0: Exit", 1, 7)
 		case 3:
-			Bottes_de_l_aventurier(player1)
+			player1 = Bottes_de_l_aventurier(player1)
 			Printfct("Bienvenu dans chez le forgeron, que voulez vous fabriquer : \n\n1: Chapeau de l’aventurier (1 Plume de Corbeau et 1 Cuir de Sanglier)\n2: Tunique de l’aventurier (2 Fourrure de loup et 1 Peau de Troll)\n3: Bottes de l’aventurier (1 Fourrure de loup et 1 Cuir de Sanglier)\n\n0: Exit", 1, 7)
 		default:
 			println("invalid")
