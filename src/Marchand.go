9bbde76560611c0203ba036cbabe117ae78ade6c
dewen <ewen.denoual@ynov.com>
2025-09-17 15:09:03 +0200

    [fix] size call

--- src/Marchand.go
+++ src/Marchand.go
@@ -20,8 +20,8 @@ func Menu_marchand(player1 Character) Character {
 			player1 = Acheter_potion_poison(player1)
 			Printfct("Bienvenu chez le marchand, que voulez vous acheter :\n\n1: Potion de vie\n2: Potion de poison\n\n0: Exit", 1, 7)
 		//case 3:
-			//fonction(player1)
-			//Printfct("", 1, 7)
+		//fonction(player1)
+		//Printfct("", 1, 7)
 		default:
 			println("invalid")
 		}
@@ -29,17 +29,21 @@ func Menu_marchand(player1 Character) Character {
 }
 
 func Acheter_potion_vie(player1 Character) Character {
-	player1.inventaire.potion += 1
-	println("Achat en cours : Potion de vie")
-	time.Sleep(3 *time.Second)
-	println("Achat effectué : Vous venez d'obtenir une potion de vie")
+	if !TotalItems(player1.inventaire) {
+		player1.inventaire.potion += 1
+		println("Achat en cours : Potion de vie")
+		time.Sleep(3 * time.Second)
+		println("Achat effectué : Vous venez d'obtenir une potion de vie")
+	} else {
+		fmt.Println("Ton inventaire est plein ! Impossible d’ajouter plus d’items.")
+	}
 	return player1
 }
 
 func Acheter_potion_poison(player1 Character) Character {
 	player1.inventaire.potion_poison += 1
 	println("Achat en cours : Potion de poison")
-	time.Sleep(3 *time.Second)
+	time.Sleep(3 * time.Second)
 	println("Achat effectué : Vous venez d'obtenir une potion de poison")
 	return player1
 }
