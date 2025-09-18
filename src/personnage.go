25aad8b10d298e42e7471fbfe4ae047e5e2f71cf
btimeo <timeo.baffreauleroux@ynov.com>
2025-09-18 12:08:16 +0200

    Mise a jour : ajout de verif

--- src/personnage.go
+++ src/personnage.go
@@ -83,21 +83,21 @@ func CharacterCreation(player1 Character) Character {
 			player1.classe = "Humain"
 			player1.pv_max = 100
 			player1.pv = player1.pv_max/2
-			Printfct("Classe Humain chose", 30,1)
+			Printfct("Vous avez choisi la classe Humain", 30,1)
 			time.Sleep(2 *time.Second)
 			return player1
 		case 2 : 
 			player1.classe = "Elfes"
 			player1.pv_max = 80
 			player1.pv = player1.pv_max/2
-			Printfct("Classe Elfes chose", 30,1)
+			Printfct("Vous avez choisi la classe Elfes", 30,1)
 			time.Sleep(2 *time.Second)
 			return player1
 		case 3 :
 			player1.classe = "Nains"
 			player1.pv_max = 120
 			player1.pv = player1.pv_max/2
-			Printfct("Classe Nains chose", 30,1)
+			Printfct("Vous avez choisi la classe Nains", 30,1)
 			time.Sleep(2 *time.Second)
 			return player1
 		}
