fbcad134575a4bf071a2bfa66af977f32757c1dd
btimeo <timeo.baffreauleroux@ynov.com>
2025-09-19 11:15:05 +0200

    Mise a jour : ajout de la potion de mana

--- src/inventaire.go
+++ src/inventaire.go
@@ -8,6 +8,7 @@ import (
 type Inventory struct {
 	potion        int
 	potion_poison int
+	potion_mana   int
 	piece_or      int
 
 	Plume_de_Corbeau int
@@ -31,6 +32,7 @@ func TotalItems(inv Inventory) bool {
 		inv.Peau_de_Troll +
 		inv.Chapeau_de_l_aventurier +
 		inv.Tunique_de_l_aventurier +
+		inv.potion_mana +
 		inv.Bottes_de_l_aventurier
 	return tot >= inv.size_max
 }
@@ -38,6 +40,7 @@ func TotalItems(inv Inventory) bool {
 func initInventory() Inventory {
 	var inventaire_player1 Inventory
 	inventaire_player1.potion = 3
+	inventaire_player1.potion_mana = 0
 	inventaire_player1.potion_poison = 0
 
 	inventaire_player1.piece_or = 10
@@ -57,8 +60,8 @@ func initInventory() Inventory {
 }
 
 func DisplayInventory(player1 Character) Character {
-	fmt.Printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nInventaire : ")
-	fmt.Printf("\n---------------------------------------------------------------\nPotion = %v\nPiece Or = %v\nPlume De Corbeau = %v\nCuir De Sanglier = %v\nFourrure De Loup = %v\nPeau De Troll = %v\nChapeau De L'Aventurier = %v\nTunique De L'aventurier = %v\nBottes De L'Aventurier = %v\n---------------------------------------------------------------\n\n", player1.inventaire.potion, player1.inventaire.piece_or, player1.inventaire.Plume_de_Corbeau, player1.inventaire.Cuir_de_Sanglier, player1.inventaire.Fourrure_de_loup, player1.inventaire.Peau_de_Troll, player1.inventaire.Chapeau_de_l_aventurier, player1.inventaire.Tunique_de_l_aventurier, player1.inventaire.Bottes_de_l_aventurier)
+	fmt.Printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+	fmt.Printf("\n-------------------- Inventaire : -----------------------------\nPotion = %v\nPotion Mana = %v\nPiece Or = %v\nPlume De Corbeau = %v\nCuir De Sanglier = %v\nFourrure De Loup = %v\nPeau De Troll = %v\nChapeau De L'Aventurier = %v\nTunique De L'aventurier = %v\nBottes De L'Aventurier = %v\n---------------------------------------------------------------\n\n", player1.inventaire.potion,player1.inventaire.potion_mana, player1.inventaire.piece_or, player1.inventaire.Plume_de_Corbeau, player1.inventaire.Cuir_de_Sanglier, player1.inventaire.Fourrure_de_loup, player1.inventaire.Peau_de_Troll, player1.inventaire.Chapeau_de_l_aventurier, player1.inventaire.Tunique_de_l_aventurier, player1.inventaire.Bottes_de_l_aventurier)
 	player1 = Equipement(player1)
 	return player1
 }
@@ -99,9 +102,18 @@ func PoisonPot(player1 Character) Character {
 	return player1
 }
 
-// func Pootion_mana(player1 Character) Character {
+func Potion_mana(player1 Character) Character {
 
-// }
+	if player1.inventaire.potion_mana > 0 {
+		player1.inventaire.potion_mana -= 1
+		player1.mana += 30
+		fmt.Printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nVous disposez de %v mana\n", player1.mana)
+
+	} else {
+		fmt.Printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nVous ne disposez pas de potion de mana dans votre inventaire.\n")
+	}
+	return player1
+}
 
 func UpgradeInventorySlot(player1 Character) Character {
 	if player1.inventaire.piece_or >= 5 {
@@ -118,7 +130,7 @@ func UpgradeInventorySlot(player1 Character) Character {
 
 func Equipement(player1 Character) Character {
 	var i int
-	Printfct("Equipement : \n\n1: Equiper tete\n2: Equiper torse\n3: Equiper botte\n\n0: Retour", 1, 20)
+	Printfct("Equipement : \n\n1: Equiper tete\n2: Equiper torse\n3: Equiper botte\n\n0: Retour", 1, 25)
 	for {
 		fmt.Scanln(&i)
 		switch i {
@@ -169,8 +181,8 @@ func Menu_objet(player1 Character) Character {
 			player1 = PoisonPot(player1)
 			return player1
 		case 3:
-			//
-			Printfct("Quel objet voulez vous utiliser : \n\n1: Potion de vie \n2: Potion de poison\n3: Potion de Mana\n\n0: Retour", 1, 20)
+			player1 = Potion_mana(player1)
+			return player1
 		default:
 			println("invalid")
 		}
